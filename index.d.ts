b81c1edbbf019b2cf3839d223576b27c4296b6b9
YeFei <316606233@qq.com>
2021-03-01 13:32:35 +0800

    d.ts

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+import { Core } from '@zenweb/core';
+import { Options } from 'koa-bodyparser';
+export { Options } from 'koa-bodyparser';
+
+export declare function setup(core: Core, options?: Options): void;
