8be324b0867430a27809c9e83fec01fbaaf86e52
YeFei <316606233@qq.com>
2021-12-21 17:45:19 +0800

    fix: ctx.request.body ts定义

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import { Core } from '@zenweb/core';
 import * as koaBody from 'koa-body';
+import { Files } from 'formidable';
 export { IKoaBodyOptions as BodyOption } from 'koa-body';
 
 export function setup(core: Core, options?: koaBody.IKoaBodyOptions) {
   core.use(koaBody(options));
+}
+
+declare module 'koa' {
+  interface Request {
+    // 不知为何 typescript 找不到 koa-body 中的 ts 定义，这里重复添加一下
+    body?: any;
+    files?: Files;
+  }
 }
